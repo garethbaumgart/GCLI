adb288afbe0bc282dba3795447de2078e6555dd8
Gareth <garethbaumgart@gmail.com>
2020-04-26 12:01:25 +1000

    Creating project structure

---
+++ GCommandLine.Test/EncryptionTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GCommandLine.Test
+{
+    public class EncryptionTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ GCommandLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GCommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
